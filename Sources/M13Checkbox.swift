741e8ad96c85ffcf3ca7f76dc279eb831c188d0a
Silin, Vasily <v.silin@itransition.com>
2016-12-19 09:38:57 +0300

    Switch back enum manager property to fileprivate. Make open M13Checkbox controller.

--- Sources/M13Checkbox.swift
+++ Sources/M13Checkbox.swift
@@ -198,7 +198,7 @@ open class M13Checkbox: UIControl {
         }
         
         /// The manager for the specific animation type.
-        public var manager: M13CheckboxController {
+        fileprivate var manager: M13CheckboxController {
             switch self {
             case .stroke:
                 return M13CheckboxStrokeController()
