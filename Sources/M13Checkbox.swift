b3984578179283e758b5f6ce016f1e8679812859
Vasili Silin <v.silin@itransition.com>
2018-01-04 10:51:08 +0300

    Remove build warnings.

--- Sources/M13Checkbox.swift
+++ Sources/M13Checkbox.swift
@@ -469,7 +469,7 @@ open class M13Checkbox: UIControl {
     }
     
     /// The type of mark to display.
-    @IBInspectable open var markType: MarkType {
+    open var markType: MarkType {
         get {
             return controller.markType
         }
